7c5363c116a1b24a9a2183f6e4be6b735b5bd46d
Assad Yousuf <ayousuf@asu.edu>
2020-05-27 16:25:37 -0700

    inital commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { LandingPage } from './LandingPage/LandingPage';
+
+function App() {
+  return (
+    <LandingPage/>
+  );
+}
+
+export default App;
---
+++ src/LandingPage/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { TopNav } from './TopNav/TopNav';
+import logo from '../assets/index.png';
+import styles from './LandingPage.module.css';
+
+export function LandingPage() {
+  return (
+   <div>
+       <TopNav/>
+       <img src={logo} className={styles.logo} alt = 'logo' />
+   </div>
+  );
+}
+
+
